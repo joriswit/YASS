9be4e96f01f576f682ba737bcfbe7010061b53ba
joriswit <github@joriswit.nl>
2019-07-07 16:51:21 +0200

    Upgraded gradle to 3.4.1 Set target SDK to 29 Migrate support library to androidx

--- app/src/main/java/yass/BoardView.java
+++ app/src/main/java/yass/BoardView.java
@@ -3,7 +3,7 @@ package yass;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
